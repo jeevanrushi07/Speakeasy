1aa8abab0ad42a491192001d4552903dac28a0bb
jeevanrushi07 <jeevanrushicreations584@gmail.com>
2026-09-19 02:39:46 +0530

    Fix profile modal import

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Link, useLocation } from "react-router";
+import { useState } from "react";
 import useAuthUser from "../hooks/useAuthUser";
 import {
   LogOutIcon,
@@ -16,8 +17,6 @@ const Navbar = () => {
   const { authUser } = useAuthUser();
   const location = useLocation();
   const isChatPage = location.pathname?.startsWith("/chat");
-
-import { useState } from "react";
   // const queryClient = useQueryClient();
   // const { mutate: logoutMutation } = useMutation({
   //   mutationFn: logout,
